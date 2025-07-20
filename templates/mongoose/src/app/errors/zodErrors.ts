98dd0ccbfa5a75cf745271d22412fee21db13e1d
Nazmul Hassan <nazmulnhb007@yahoo.com>
2025-07-20 13:42:32 +0600

    update(errors): updated zod error handler and get all users permission

--- templates/mongoose/src/app/errors/zodErrors.ts
+++ templates/mongoose/src/app/errors/zodErrors.ts
@@ -3,18 +3,15 @@ import type { ZodError } from 'zod';
 import type { IErrorResponse, IErrorSource } from '../types/interfaces';
 
 /** * Processes Zod Validation Errors and returns a structured response. */
-export const handleZodErrors = (
-	error: ZodError,
-	stack?: string,
-): IErrorResponse => {
+export const handleZodErrors = (error: ZodError, stack?: string): IErrorResponse => {
 	const errorSource: IErrorSource[] = error.issues.map((zodIssue) => {
 		const path = zodIssue.path.join('.');
 		let message = zodIssue.message;
 
 		switch (zodIssue.code) {
-			case 'invalid_type':
-				message = `Expected ${zodIssue.expected} for “${path}” but received “${zodIssue.input}”!`;
-				break;
+			// case 'invalid_type':
+			// 	message = `Expected ${zodIssue.expected} for “${path}” but received “${zodIssue.input}”!`;
+			// 	break;
 			case 'invalid_value':
 				message = `Invalid value for “${path}”. Expected one of: “${joinArrayElements(zodIssue.values)}” but received “${zodIssue.input}”!`;
 				break;
--- templates/mongoose/src/app/modules/user/user.routes.ts
+++ templates/mongoose/src/app/modules/user/user.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+import { USER_ROLES } from '../../constants';
+import authorizeUser from '../../middlewares/authorizeUser';
 import { userControllers } from './user.controllers';
 
 const router = Router();
 
-router.get('/', userControllers.getAllUsers);
+router.get('/', authorizeUser(...Object.values(USER_ROLES)), userControllers.getAllUsers);
 
 export const userRoutes = router;
