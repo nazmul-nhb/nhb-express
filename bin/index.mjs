0bef609468b13b8a6d29d7c839447423519c49e0
Nazmul Hassan <nazmulnhb007@yahoo.com>
2025-07-20 16:04:19 +0600

    update(npm): modified code to display live npm feed

--- bin/index.mjs
+++ bin/index.mjs
@@ -249,8 +249,16 @@ async function installDeps(manager, cwd, deps, devDeps) {
 		await execa('pnpm', ['add', ...deps], { cwd, stdout: 'inherit' });
 		await execa('pnpm', ['add', '-D', ...devDeps], { cwd, stdout: 'inherit' });
 	} else if (manager === 'npm') {
-		await execa('npm', ['install', ...deps], { cwd, stdout: 'inherit' });
-		await execa('npm', ['install', '-D', ...devDeps], { cwd, stdout: 'inherit' });
+		await execa('npm', ['install', '--progress', ...deps], {
+			cwd,
+			stdout: 'inherit',
+			stderr: 'inherit',
+		});
+		await execa('npm', ['install', '--progress', '-D', ...devDeps], {
+			cwd,
+			stdout: 'inherit',
+			stderr: 'inherit',
+		});
 	} else if (manager === 'yarn') {
 		await execa('yarn', ['add', ...deps], { cwd, stdout: 'inherit' });
 		await execa('yarn', ['add', '--dev', ...devDeps], { cwd, stdout: 'inherit' });
