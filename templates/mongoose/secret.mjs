a954bd770b105b027954de60e49ce263889be7f6
Nazmul Hassan <nazmulnhb007@yahoo.com>
2025-07-20 22:53:33 +0600

    update(cors/comment): updated cors and added comment in secret generator

--- templates/mongoose/secret.mjs
+++ templates/mongoose/secret.mjs
@@ -2,6 +2,11 @@
 
 import { randomBytes } from 'crypto';
 
+/** 
+ * * Generates a random secret key for use in the application (e.g. jwt secrets).
+ * * Run `node secret.mjs` in the terminal opened in the project directory.
+ */
+
 const secret = randomBytes(64).toString('hex');
 
-console.info(secret)
+console.info(secret);
--- templates/mongoose/src/app/configs/cors.ts
+++ templates/mongoose/src/app/configs/cors.ts
@@ -4,20 +4,18 @@ export const corsOptions: CorsOptions = {
 	origin: (origin, callback) => {
 		const allowedOrigins = [
 			/^http:\/\/localhost:\d+$/,
-			/^http:\/\/192\.168\.0\.1:\d+$/,
+			/^http:\/\/192\.168\.0\.\d+:\d+$/,
 		];
 
 		if (
 			!origin ||
 			allowedOrigins.some((pattern) =>
-				typeof pattern === 'string' ?
-					pattern === origin
-				:	pattern.test(origin),
+				typeof pattern === 'string' ? pattern === origin : pattern.test(origin),
 			)
 		) {
 			callback(null, true);
 		} else {
-			callback(new Error('Not allowed by CORS'));
+			callback(new Error('Not Allowed by CORS!'));
 		}
 	},
 	// origin: '*',
