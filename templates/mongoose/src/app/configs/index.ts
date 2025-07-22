83bcfeed7602ff350401d4ead1a7d41d874a95aa
Nazmul Hassan <nazmulnhb007@yahoo.com>
2025-07-22 12:25:47 +0600

    update(dotenv): supress all logs except errors from dotenv

--- templates/mongoose/src/app/configs/index.ts
+++ templates/mongoose/src/app/configs/index.ts
@@ -3,13 +3,11 @@ import type { LooseLiteral } from 'nhb-toolbox/utils/types';
 import path from 'path';
 import type { ms } from '../../index';
 
-dotenv.config({ path: path.join(process.cwd(), '.env') });
+dotenv.config({ path: path.join(process.cwd(), '.env'), quiet: true });
 
 export default {
 	port: process.env.PORT || 4242,
-	NODE_ENV: process.env.NODE_ENV as LooseLiteral<
-		'development' | 'production'
-	>,
+	NODE_ENV: process.env.NODE_ENV as LooseLiteral<'development' | 'production'>,
 	mongoUri: process.env.MONGO_URI as string,
 	saltRounds: Number(process.env.SALT_ROUNDS),
 	accessSecret: process.env.JWT_ACCESS_SECRET as string,
