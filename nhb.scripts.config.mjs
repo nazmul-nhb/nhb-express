ff28ace9e7257c17f6c314f9df5da8eb346e7634
Nazmul Hassan <nazmulnhb007@yahoo.com>
2025-08-08 15:39:11 +0600

    update(husky+release): itegrated husky and released with import alias fix

--- nhb.scripts.config.mjs
+++ nhb.scripts.config.mjs
@@ -3,16 +3,16 @@
 import { defineScriptConfig } from 'nhb-scripts';
 
 export default defineScriptConfig({
-    format: {
-        args: ['--write'],
-        files: ['bin',],
-        ignorePath: '.prettierignore',
-    },
-    commit: {
-        runFormatter: true,
-    },
-    count: {
-        defaultPath: 'bin',
-        excludePaths: ['node_modules', '.github']
-    },
+	format: {
+		args: ['--write'],
+		files: ['bin'],
+		ignorePath: '.prettierignore',
+	},
+	commit: {
+		runFormatter: false,
+	},
+	count: {
+		defaultPath: 'bin',
+		excludePaths: ['node_modules', '.github'],
+	},
 });
